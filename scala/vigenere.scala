136244fc6c7a696b0a5e34a006caf42c5f92e194
Vinicius Resende <viniatresnde@gmail.com>
2020-02-14 23:13:52 -0300

    Finaliza função de cifrar e descifrar em Scala

--- scala/vigenere.scala
+++ scala/vigenere.scala
@@ -1,34 +1,39 @@
-import scala.io.Source
-
 def repeatUntilSize(str: String, size: Int) : String = {
   val n = size / str.length
   val modulo = size % str.length
 
   (str * n) ++ str.slice(0, modulo)
 }
-def cipherChar(tk:(Char,Char)) : Char = {
+def cipherChar(tk: (Char,Char)) : Char = {
   val (t,k) = tk
-  if(t == ' '){
-    ' '
-  }else{
-    ((t + k) % 26 + 65).toChar
-  }
+  ((t + k) % 26 + 65).toChar
 }
-/*
-No momento so funciona com inputs sem espaço e com
-todas as letras maiusculas.
-o problema é que com espaços a chave e o texto desalinham, mesmo desconsiderando espaços'
-*/
-def cipher(text: String, key: String="somerandomstuff") : String = {
-  val key_array   = repeatUntilSize(key, text.length)//.map(_.toByte)
 
-  text.zip(key_array)
+def cipher(text: String, key: String="somerandomstuff") : String = {
+  val transText = text.filterNot(_.isWhitespace)
+                      .toUpperCase
+  val transKey  = key.filterNot(_.isWhitespace)
+                      .toUpperCase
+  val keyArray  = repeatUntilSize(transKey, transText.length)
+
+  transText.zip(keyArray)
       .map(cipherChar)
       .mkString
 }
-/*
-def decipher(text: String, key: String = "somerandomstuff") : String = {
-  val ascii_array = text.map(_.toByte)
-  val ascii_key   = repeatUntilSize(key, text.length ).map(_.toByte)
+
+def decipherChar(tk: (Char,Char)) : Char = {
+  val (t,k) = tk
+  ((t - k + 26) % 26 + 65).toChar
 }
-*/
+
+def decipher(text: String, key: String = "somerandomstuff") : String = {
+  val transText = text.filterNot(_.isWhitespace)
+                      .toUpperCase
+  val transKey  = key.filterNot(_.isWhitespace)
+                      .toUpperCase
+  val keyArray  = repeatUntilSize(transKey, transText.length)
+
+  transText.zip(keyArray)
+           .map(decipherChar)
+           .mkString
+}
