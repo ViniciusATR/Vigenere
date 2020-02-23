b7ff8582f24bec921ee3a47cc85920e85cd75186
Vinicius Resende <viniatresnde@gmail.com>
2020-02-23 01:03:01 -0300

    Expande tamanho do alfabeto e finaliza versão scala

--- scala/vigenere.scala
+++ scala/vigenere.scala
@@ -1,39 +1,55 @@
-def repeatUntilSize(str: String, size: Int) : String = {
-  val n = size / str.length
-  val modulo = size % str.length
+import scala.io.Source
 
-  (str * n) ++ str.slice(0, modulo)
-}
-def cipherChar(tk: (Char,Char)) : Char = {
-  val (t,k) = tk
-  ((t + k) % 26 + 65).toChar
-}
+object VigenereCipher {
 
-def cipher(text: String, key: String="somerandomstuff") : String = {
-  val transText = text.filterNot(_.isWhitespace)
-                      .toUpperCase
-  val transKey  = key.filterNot(_.isWhitespace)
-                      .toUpperCase
-  val keyArray  = repeatUntilSize(transKey, transText.length)
+  def repeatUntilSize(str: String, size: Int) : String = {
+    val n = size / str.length
+    val modulo = size % str.length
 
-  transText.zip(keyArray)
-      .map(cipherChar)
-      .mkString
-}
+    (str * n) ++ str.slice(0, modulo)
+  }
 
-def decipherChar(tk: (Char,Char)) : Char = {
-  val (t,k) = tk
-  ((t - k + 26) % 26 + 65).toChar
-}
+  def cipherChar(tk: (Char,Char)) : Char = {
+    val (t,k) = tk
+    ((t + k) % 91 + 32).toChar
+  }
 
-def decipher(text: String, key: String = "somerandomstuff") : String = {
-  val transText = text.filterNot(_.isWhitespace)
-                      .toUpperCase
-  val transKey  = key.filterNot(_.isWhitespace)
-                      .toUpperCase
-  val keyArray  = repeatUntilSize(transKey, transText.length)
+  def cipher(text: String, key: String ) : String = {
+    val transText = text.map((c)=>(c-32).toChar)
+    val transKey  = key.map((c)=>(c-32).toChar)
+    val keyArray  = repeatUntilSize(transKey, transText.length)
 
-  transText.zip(keyArray)
-           .map(decipherChar)
-           .mkString
+    transText.zip(keyArray)
+        .map(cipherChar)
+        .mkString
+  }
+
+  def decipherChar(tk: (Char,Char)) : Char = {
+    val (t,k) = tk
+    ((t - k + 91) % 91 + 32).toChar
+  }
+
+  def decipher(text: String, key: String ) : String = {
+    val transText = text.map((c)=>(c-32).toChar)
+    val transKey  = key.map((c)=>(c-32).toChar)
+    val keyArray  = repeatUntilSize(transKey, transText.length)
+
+    transText.zip(keyArray)
+             .map(decipherChar)
+             .mkString
+  }
+  
+  def main(args: Array[String]): Unit = {
+    if (args.size == 0)
+      println("Por favor utilize 3 argumentos de entrada:\nmodo (c/d)\narquivo_texto\nchave")
+    else{
+      val mode = args(0)
+      val txt  = Source.fromFile(args(1)).getLines.mkString
+      val key  = args(2)
+      mode match {
+        case "c" => println(cipher(txt,key))
+        case "d" => println(decipher(txt,key))
+      }
+    }   
+  }
 }
